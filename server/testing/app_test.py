22091bfc1b62ca8196aa86b635e3548525681f09
Stiveys <stevomwaura@gmail.com>
2025-04-02 15:33:57 +0300

    new

--- server/testing/app_test.py
+++ server/testing/app_test.py
@@ -7,11 +7,11 @@ app.secret_key = b'a\xdb\xd2\x13\x93\xc1\xe9\x97\xef2\xe3\x004U\xd1Z'
 
 class TestApp:
     '''Flask API in app.py'''
-    
+
     def test_can_only_access_member_only_while_logged_in(self):
         '''allows logged in users to access member-only article index at /members_only_articles.'''
         with app.test_client() as client:
-            
+
             client.get('/clear')
 
             user = User.query.first()
@@ -30,7 +30,7 @@ class TestApp:
     def test_member_only_articles_shows_member_only_articles(self):
         '''only shows member-only articles at /members_only_articles.'''
         with app.test_client() as client:
-            
+
             client.get('/clear')
 
             user = User.query.first()
@@ -45,7 +45,7 @@ class TestApp:
     def test_can_only_access_member_only_article_while_logged_in(self):
         '''allows logged in users to access full member-only articles at /members_only_articles/<int:id>.'''
         with app.test_client() as client:
-            
+
             client.get('/clear')
 
             user = User.query.first()
@@ -53,7 +53,7 @@ class TestApp:
                 'username': user.username
             })
 
-            article_id = Article.query.with_entities(Article.id).first()[0]
+            article_id = Article.query.filter(Article.is_member_only == True).with_entities(Article.id).first()[0]
 
             response = client.get(f'/members_only_articles/{article_id}')
             assert(response.status_code == 200)
